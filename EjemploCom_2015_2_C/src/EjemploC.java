5e10eb60188053f051cb22b2d4e4f40249ade9df
Juanda <Juanda@192.168.1.51>
2017-10-10 17:36:12 -0500

    asd

--- EjemploCom_2015_2_C/src/EjemploC.java
+++ EjemploCom_2015_2_C/src/EjemploC.java
@@ -876,7 +876,7 @@ public class EjemploC extends PApplet {
 	@Override
 	public void mouseClicked() {
 
-// vamosa  vers		
+// vamosa  vers	s	
 		
 		if (comC.turno==3 && comC.cicloJuego==0) {
 			comC.enviar("INICIARjUEGO");
